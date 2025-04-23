4b031bdbe05bb39b07c3a3628e3864101363da91
brunaporato <brunaporato@gmail.com>
2025-04-23 07:35:48 -0300

    feat: create input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#fff",
+        width: "100%",
+        height: 48,
+        paddingHorizontal: 16,
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: "#C3C5CB",
+    }
+})
