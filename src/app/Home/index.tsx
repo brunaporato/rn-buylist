68d14cdf76d75547582a7bce32dedfa7fd70d9a8
brunaporato <brunaporato@gmail.com>
2025-04-28 07:44:01 -0300

    feat: having change state for filters at home screen

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
 import { FilterStatus } from "@/types/filterStatus";
 import { Item } from "@/components/Item";
+import { useState } from "react";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 const ITEMS = [
@@ -27,19 +28,22 @@ const ITEMS = [
 ];
 
 export function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING)
+  const [description, setDescription] = useState("")
+
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
+        <Input placeholder="O que você precisa comprar?" onChangeText={setDescription} />
         <Button title="Adicionar" />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter key={status} status={status} isActive={status === filter} onPress={() => setFilter(status)} />
           ))}
 
           <TouchableOpacity activeOpacity={0.8} style={styles.clearButton}>
